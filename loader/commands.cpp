7e0dc0c234dcd26281a5eb186f56c51f6519199b
Aaron Dewey <aaron@scottsmarketplace.com>
2015-02-01 22:47:29 -0700

    support for stdin

--- loader/commands.cpp
+++ loader/commands.cpp
@@ -9,15 +9,18 @@ void InputCommand(int argc, char *argv[])
 }
 
 FILE *originalStdOut = NULL;
+FILE *originalStdIn = NULL;
 void InitConsole()
 {
 	/* attach our console to our injector's console */
 	AttachConsole(GetProcessId(L"injector.exe"));
 	originalStdOut = freopen("CONOUT$", "w", stdout);
+	originalStdIn = freopen("CONIN$", "r", stdin);
 }
 
 void RemoveConsole()
 {
 	freopen("CONOUT$", "w", originalStdOut);
+	freopen("CONIN$", "r", originalStdIn);
 	FreeConsole();
 }
