b81ae78e83668ba942da355b2e83d8839577bb4b
Aaron Dewey <aaron@scottsmarketplace.com>
2015-02-26 13:56:49 -0700

    reverted some changes due to debug failure, moved update function to main thread where it belongs

--- tracker/tracker.cpp
+++ tracker/tracker.cpp
@@ -79,83 +79,15 @@ void SendMessage(string message){
 	SendToServer.push_back(message);
 }
 
-
-DWORD WINAPI updateServer(void *pThreadArgument)
-{
-	while (bAttached && gbAttached)
-	{
-		if (bUpdateRequired && GetPlayerEntity())
-		{
-			Player *pPlayer = GetPlayerEntity();
-			PlayerManager *pPlayerManager = GetPlayerManager();
-			/* craft our json object to send to the server */
-			/* craft our item array */
-			char itembuffer[1024] = { 0 };
-			strcat_s(itembuffer, 1024 - 1, "[");
-			char itemidbuffer[0x32];
-			for (int i = 0; i < 0x15A; i++)
-			{
-				ZeroMemory(itemidbuffer, 0x32);
-				if (pPlayer->_items[i])
-				{
-					sprintf_s(itemidbuffer, 0x32, "%d,", i + 1);
-					strcat_s(itembuffer, 1024 - 1, itemidbuffer);
-				}
-			}
-			/* replace our trailing comma with a closing bracket */
-			itembuffer[strlen(itembuffer) - 1] = ']';
-			if (strlen(itembuffer) == 1){
-				itembuffer[0] = '[';
-				itembuffer[1] = ']';
-			}
-			/* craft our trinket array */
-			char trinketbuffer[1024] = { 0 };
-			strcat_s(trinketbuffer, 1024, "[");
-			char trinketidbuffer[0x32];
-			ZeroMemory(trinketidbuffer, 0x32 - 1);
-			sprintf_s(trinketidbuffer, 0x32 - 1, "%d,", pPlayer->_trinket1ID);
-			strcat_s(trinketbuffer, 1024 - 1, trinketidbuffer);
-			ZeroMemory(trinketidbuffer, 0x32 - 1);
-			sprintf_s(trinketidbuffer, 0x32 - 1, "%d,", pPlayer->_trinket2ID);
-			strcat_s(trinketbuffer, 1024 - 1, trinketidbuffer);
-			/* replace our trailing comma with a closing bracket */
-			trinketbuffer[strlen(trinketbuffer) - 1] = ']';
-
-			/* craft our pocket array */
-			char pocketbuffer[1024] = { 0 };
-			strcat_s(pocketbuffer, 1024 - 1, "[");
-			char pocketidbuffer[0x32];
-			ZeroMemory(pocketidbuffer, 0x32 - 1);
-			sprintf_s(pocketidbuffer, 0x32 - 1, "{\"id\": %d, \"is_card\": %d},", pPlayer->_pocket1ID, pPlayer->_pocket1isCard);
-			strcat_s(pocketbuffer, 1024 - 1, pocketidbuffer);
-			ZeroMemory(pocketidbuffer, 0x32 - 1);
-			sprintf_s(pocketidbuffer, 0x32 - 1, "{\"id\": %d, \"is_card\": %d},", pPlayer->_pocket2ID, pPlayer->_pocket2isCard);
-			strcat_s(pocketbuffer, 1024 - 1, pocketidbuffer);
-			/* replace our trailing comma with a closing bracket */
-			pocketbuffer[strlen(pocketbuffer) - 1] = ']';
-
-			char buffer2[2048] = { 0 };
-			sprintf_s(buffer2, 2048 - 1, "{\"stream_key\": \"%s\",\"character\": \"%s\", \"characterid\": \"%d\", \"seed\": \"%s\", \"floor\": \"%d\", \"altfloor\": \"%d\", \"curses\": \"%d\", \"guppy\": \"%d\", \"lof\": \"%d\", \"charges\": \"%d\", \"speed\": \"%0.2f\", \"shotspeed\": \"%0.2f\", \"tearrate\": %d, \"damage\": \"%0.2f\", \"luck\": \"%0.2f\", \"range\": \"%0.2f\", \"coins\": \"%d\", \"bombs\": \"%d\", \"keys\": \"%d\", \"items\": %s, \"trinkets\": %s, \"pockets\": %s}",
-				gTrackerID, pPlayer->_characterName, pPlayer->_charID, pPlayerManager->_startSeed, pPlayerManager->_floorNo, pPlayerManager->_alternateFloor, pPlayerManager->i_curses, pPlayer->_nGuppyItems, pPlayer->_nFlyItems, pPlayer->_charges, pPlayer->_speed, pPlayer->_shotspeed, pPlayer->_tearrate, pPlayer->_damage, pPlayer->_luck, pPlayer->_range, pPlayer->_numCoins, pPlayer->_numBombs, pPlayer->_numKeys, itembuffer, trinketbuffer, pocketbuffer);
-			SendMessage((string)buffer2);
-			bUpdateRequired = 0;
-		}
-		Sleep(1000);
-	}
-	return 0;
-}
-
 PAPI VOID InitPlugin()
 {
 	bAttached = true;
-
 	trackerLogFile.open("gemini/tracker_log.txt");
 	Log("Tracker Started");
 	IniReadString("tracker", "key", gTrackerID);
 	string trackerLog = "Tracker ID: ";
 	trackerLog.append(gTrackerID);
 	Log(trackerLog);
-	CreateThread(NULL, 0, updateServer, NULL, 0L, NULL);
 	CreateThread(NULL, 0, socketHandler, NULL, 0L, NULL);
 }
 
@@ -163,6 +95,66 @@ PAPI VOID UnInitPlugin(VOID)
 {
 	bAttached = false;
 	trackerLogFile.close();
+}
+
+
+
+void updateServer()
+{
+	PlayerManager *pPlayerManager = GetPlayerManager();
+	if (!pPlayerManager) return;
+	Player *pPlayer = GetPlayerEntity();
+	if (!pPlayer) return;
+	/* craft our json object to send to the server */
+	/* craft our item array */
+	char itembuffer[1024] = { 0 };
+	strcat_s(itembuffer, 1024 - 1, "[");
+	char itemidbuffer[0x32];
+	for (int i = 0; i < 0x15A; i++)
+	{
+		ZeroMemory(itemidbuffer, 0x32);
+		if (pPlayer->_items[i])
+		{
+			sprintf_s(itemidbuffer, 0x32, "%d,", i + 1);
+			strcat_s(itembuffer, 1024 - 1, itemidbuffer);
+		}
+	}
+	/* replace our trailing comma with a closing bracket */
+	itembuffer[strlen(itembuffer) - 1] = ']';
+	if (strlen(itembuffer) == 1){
+		itembuffer[0] = '[';
+		itembuffer[1] = ']';
+	}
+	/* craft our trinket array */
+	char trinketbuffer[1024] = { 0 };
+	strcat_s(trinketbuffer, 1024, "[");
+	char trinketidbuffer[0x32];
+	ZeroMemory(trinketidbuffer, 0x32 - 1);
+	sprintf_s(trinketidbuffer, 0x32 - 1, "%d,", pPlayer->_trinket1ID);
+	strcat_s(trinketbuffer, 1024 - 1, trinketidbuffer);
+	ZeroMemory(trinketidbuffer, 0x32 - 1);
+	sprintf_s(trinketidbuffer, 0x32 - 1, "%d,", pPlayer->_trinket2ID);
+	strcat_s(trinketbuffer, 1024 - 1, trinketidbuffer);
+	/* replace our trailing comma with a closing bracket */
+	trinketbuffer[strlen(trinketbuffer) - 1] = ']';
+
+	/* craft our pocket array */
+	char pocketbuffer[1024] = { 0 };
+	strcat_s(pocketbuffer, 1024 - 1, "[");
+	char pocketidbuffer[0x32];
+	ZeroMemory(pocketidbuffer, 0x32 - 1);
+	sprintf_s(pocketidbuffer, 0x32 - 1, "{\"id\": %d, \"is_card\": %d},", pPlayer->_pocket1ID, pPlayer->_pocket1isCard);
+	strcat_s(pocketbuffer, 1024 - 1, pocketidbuffer);
+	ZeroMemory(pocketidbuffer, 0x32 - 1);
+	sprintf_s(pocketidbuffer, 0x32 - 1, "{\"id\": %d, \"is_card\": %d},", pPlayer->_pocket2ID, pPlayer->_pocket2isCard);
+	strcat_s(pocketbuffer, 1024 - 1, pocketidbuffer);
+	/* replace our trailing comma with a closing bracket */
+	pocketbuffer[strlen(pocketbuffer) - 1] = ']';
+
+	char buffer2[2048] = { 0 };
+	sprintf_s(buffer2, 2048 - 1, "{\"stream_key\": \"%s\",\"character\": \"%s\", \"characterid\": \"%d\", \"seed\": \"%s\", \"floor\": \"%d\", \"altfloor\": \"%d\", \"curses\": \"%d\", \"guppy\": \"%d\", \"lof\": \"%d\", \"charges\": \"%d\", \"speed\": \"%0.2f\", \"shotspeed\": \"%0.2f\", \"tearrate\": %d, \"damage\": \"%0.2f\", \"luck\": \"%0.2f\", \"range\": \"%0.2f\", \"coins\": \"%d\", \"bombs\": \"%d\", \"keys\": \"%d\", \"items\": %s, \"trinkets\": %s, \"pockets\": %s}",
+		gTrackerID, pPlayer->_characterName, pPlayer->_charID, pPlayerManager->_startSeed, pPlayerManager->_floorNo, pPlayerManager->_alternateFloor, pPlayerManager->i_curses, pPlayer->_nGuppyItems, pPlayer->_nFlyItems, pPlayer->_charges, pPlayer->_speed, pPlayer->_shotspeed, pPlayer->_tearrate, pPlayer->_damage, pPlayer->_luck, pPlayer->_range, pPlayer->_numCoins, pPlayer->_numBombs, pPlayer->_numKeys, itembuffer, trinketbuffer, pocketbuffer);
+	SendMessage((string)buffer2);
 }
 
 bool bShouldUpdate = false;
@@ -186,15 +178,15 @@ PAPI VOID PostChangeCoins(int ret)
 	bShouldUpdate = true;
 }
 
-DWORD dwFrameCount = 30 * 60;
+DWORD dwFrameCount = 60 * 5;
 PAPI VOID OnGameUpdate()
 {
 	/* limit updates to once every 30 frames, then wait to update again until we need to */
-	if (dwFrameCount > 60 && bShouldUpdate && !bUpdateRequired)
+	if (dwFrameCount > (60 * 5) && bShouldUpdate)
 	{
 		bShouldUpdate = false;
-		bUpdateRequired = true;
 		dwFrameCount = 0;
+		updateServer();
 	}
 	dwFrameCount++;
 }
