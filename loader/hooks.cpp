5134d03774d1c73de5587e35247153f099bde085
Aaron Dewey <aaron@scottsmarketplace.com>
2015-03-02 20:05:52 -0700

    getting more comfortable with sigs!

--- loader/hooks.cpp
+++ loader/hooks.cpp
@@ -79,11 +79,8 @@ void InitHooks()
 	gdwGameUpdate = dwFindPattern(gdwBaseAddress, gdwBaseSize,
 		(BYTE*)"\x55\x8b\xec\x51\x53\x56\x8b\x35\x00\x00\x00\x00\x57",
 		"xxxxxxxx????x");
-
 	if (gdwGameUpdate)
-	{
 		original_gameUpdate = (void(__cdecl *)())DetourFunction((PBYTE)gdwGameUpdate, (PBYTE)gameUpdate);
-	}
 
 	gdwAddCollectible = dwFindPattern(gdwBaseAddress, gdwBaseSize,
 		(BYTE*)"\x55\x8B\xEC\x83\xE4\xF8\x6A\xFF\x68\x00\x00\x00\x00\x64\xA1\x00\x00\x00\x00\x50"
@@ -91,31 +88,29 @@ void InitHooks()
 		"\x00\x00\x33\xC4\x50\x8D\x84\x24\x88\x00\x00\x00\x64\xA3\x00\x00\x00\x00\x8B\x75"
 		"\x08\x8B\xD9\x89\x5C\x24\x1C\x89\x74\x24\x24\x81\xFE",
 		"xxxxxxxxx????xx????xxxxx????xxxxxxxxxx????xxxxxxxxxx????xxxxxxxxxxxx");
-
 	if (gdwAddCollectible)
-	{
 		original_addCollectible = (int(__fastcall *)(Player *, int, int, int, int))DetourFunction((PBYTE)gdwAddCollectible, (PBYTE)addCollectible);
-	}
 	
-	dwChangeKeys = gdwBaseAddress + 0xCEBE0;
+	dwChangeKeys = *(DWORD*)(dwFindPattern(gdwBaseAddress, gdwBaseSize,
+		(PBYTE)"\xE8\x00\x00\x00\x00\x8B\x74\x24\x14\xC6\x44"
+		"\x24\x00\x00\xE9\x00\x00\x00\x00\x83\xF8\x03",
+		"x????xxxxxxx??x????xxx") + 1);
 	if (dwChangeKeys)
-	{
 		original_changeKeys = (int(__fastcall *)(Player *, int, int))DetourFunction((PBYTE)dwChangeKeys, (PBYTE)changeKeys);
-	}
 
-	dwChangeBombs = gdwBaseAddress + 0xCEB90;
+	dwChangeBombs = *(DWORD*)(dwFindPattern(gdwBaseAddress, gdwBaseSize,
+		(PBYTE)"\xE8\x00\x00\x00\x00\x8B\x74\x24\x14\xC6\x44\x24\x00\x00"
+		"\xE9\x00\x00\x00\x00\x83\xF8\x02\x0F\x85\x00\x00\x00\x00\x50",
+		"x????xxxxxxx??x????xxxxx????x") + 1);
 	if (dwChangeBombs)
-	{
 		original_changeBombs = (int(__fastcall *)(Player *, int, int))DetourFunction((PBYTE)dwChangeBombs, (PBYTE)changeBombs);
-	}
 
 	dwChangeCoins = dwFindPattern(gdwBaseAddress, gdwBaseSize,
 		(PBYTE)"\x55\x8B\xEC\x83\xE4\xF8\x83\xEC\x0C\x53\x8B\xD8",
-		"xxxxxxxxxxxx");// gdwBaseAddress + 0xCEAB0;
+		"xxxxxxxxxxxx");
 	if (dwChangeCoins)
-	{
 		original_changeCoins = (DWORD)DetourFunction((PBYTE)dwChangeCoins, (PBYTE)changeCoins);
-	}
+
 }
 
 void RemoveHooks()
